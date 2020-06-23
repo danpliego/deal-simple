d0565785bcf23be31328e23897059dcfe1b84019
El dano <danielpliego@gmail.com>
2020-06-23 14:09:35 -0500

    add reserve modal and rearrange css

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,10 +7,19 @@ $( document ).ready(function() {
       $(this).toggleClass('btn-primary');
       $(this).toggleClass('btn-default');
       $(this).children('.view-more-text').toggleClass('d-none');
-    })
+    });
 
     $('.dropdown-menu').on("click.bs.dropdown", function (e) {
       e.stopPropagation();
     });
-});
 
+    $('.toggle-reserve').click(function() {
+      $('.reserve-modal').fadeIn();
+    });
+
+
+    $('.reserve-modal-backdrop').click(function() {
+      $('.reserve-modal').fadeOut();
+    });
+
+});
